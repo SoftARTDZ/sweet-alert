3095cb4475df69ef5d1cf0f14c4c45c6c052acc4
Mohammed DS <92916932+mohamedds-12@users.noreply.github.com>
2023-03-05 17:10:52 +0100

    Added error session key & passing timer value when working with session()

--- src/ToSweetAlert.php
+++ src/ToSweetAlert.php
@@ -18,13 +18,17 @@ class ToSweetAlert
     {
         if ($request->session()->has('success')) {
             alert()->success(
-                is_array($request->session()->get('success')) // if array is passed, put the 1st param as a title
-                    ? $request->session()->get('success')[0]
-                    : $request->session()->get('success') // else put the whole value as title
+                is_array($request->session()->get('success'))
+                    ? $request->session()->get('success')[0] // if array is passed, put the 1st param as a title
+                    : $request->session()->get('success')    // else put the whole value as title
             ,
-                is_array($request->session()->get('success')) // if array is passed, put the 1st param as a description
-                    ? $request->session()->get('success')[1]
-                    : null                                      // else put nothing as description
+                is_array($request->session()->get('success'))
+                    ? $request->session()->get('success')[1] // if array is passed, put the 2st param as a description
+                    : null                                   // else put nothing as description
+            )->autoClose(
+                is_array($request->session()->get('success')) && isset($request->session()->get('success')[2])
+                    ? $request->session()->get('success')[2] // if array is passed, put the 3nd param as a auto_close_timer value
+                    : config('sweetalert.timer')             // else put the default auto_close_timer value
             );
         }
 
@@ -37,6 +41,10 @@ class ToSweetAlert
                 is_array($request->session()->get('info'))
                     ? $request->session()->get('info')[1]
                     : null
+            )->autoClose(
+                is_array($request->session()->get('info')) && isset($request->session()->get('info')[2])
+                    ? $request->session()->get('info')[2]
+                    : config('sweetalert.timer')
             );
         }
 
@@ -49,6 +57,10 @@ class ToSweetAlert
                 is_array($request->session()->get('warning'))
                     ? $request->session()->get('warning')[1]
                     : null
+            )->autoClose(
+                is_array($request->session()->get('warning')) && isset($request->session()->get('warning')[2])
+                    ? $request->session()->get('warning')[2]
+                    : config('sweetalert.timer')
             );
         }
 
@@ -61,6 +73,26 @@ class ToSweetAlert
                 is_array($request->session()->get('question'))
                     ? $request->session()->get('question')[1]
                     : null
+            )->autoClose(
+                is_array($request->session()->get('question')) && isset($request->session()->get('question')[2])
+                    ? $request->session()->get('question')[2]
+                    : config('sweetalert.timer')
+            );
+        }
+
+        if ($request->session()->has('error')) {
+            alert()->error(
+                is_array($request->session()->get('error'))
+                    ? $request->session()->get('error')[0]
+                    : $request->session()->get('error')
+            ,
+                is_array($request->session()->get('error'))
+                    ? $request->session()->get('error')[1]
+                    : null
+            )->autoClose(
+                is_array($request->session()->get('error')) && isset($request->session()->get('error')[2])
+                    ? $request->session()->get('error')[2]
+                    : config('sweetalert.timer')
             );
         }
 
